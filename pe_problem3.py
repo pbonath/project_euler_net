172bcada137fabc57e931ac78a7ca20c45fce522
pbonath <112349433+pbonath@users.noreply.github.com>
2023-08-25 10:46:15 +0200

    Added solution for problem 3

---
+++ pe_problem3.py
@@ -0,0 +1,30 @@
+"""
+Problem 3 "Largest Prime Factor" from projecteuler.net
+"""
+#constants
+NUMBER = 600851475143
+#variables
+prime_factors: list[int] = []
+divider: int = 2
+quotient: int = NUMBER
+
+# runs as long as the quotient is larger than one
+while True:
+
+    # runs as long as quotient/divider is a natural number
+    while True:
+        # check if quotient/divider is a natural number
+        if quotient%divider == 0:
+            quotient = quotient/divider #get new quotient
+            prime_factors.append(divider)   #store divider in list of prime factors
+            divider = 2 #reset divider
+            break
+        else:
+            divider += 1    #increase divider and try again
+
+    #check if quotient is larger than  one
+    if quotient <= 1:
+        break
+
+print (prime_factors)
+print (f'The largest prime factor of {NUMBER} is {prime_factors[-1]}.')
